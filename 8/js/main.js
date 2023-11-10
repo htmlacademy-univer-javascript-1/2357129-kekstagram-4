1e2ddf77a698df1276ed42aea1f268f5bcf0eb69
Keks <keks@htmlacademy.ru>
2023-11-10 20:14:42 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,5 @@
+import './data.js';
+import {PHOTOS_COUNT, createImage } from './data.js';
+import {createPictures} from './pictures.js';
+
+createPictures(Array.from( {length: PHOTOS_COUNT}, createImage));
