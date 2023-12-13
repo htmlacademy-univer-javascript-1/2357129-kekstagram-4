11eac7bf5e7e6c093f3714ab61430522df319776
Горшков Георгий <steamgames228@list.ru>
2023-12-13 19:24:57 +0500

    Выполнил "Перламутровые пуговицы" и рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-import { renderPhotos } from './pictures.js';
 import {uploadForm} from './formUpload.js';
 import {setData} from './fetch.js';
-import {showUnloadingErrorMessage} from './utils.js';
+import {onRecieveSuccess, showUnloadingErrorMessage} from './upload-data.js';
 
-setData(renderPhotos,
+setData(onRecieveSuccess,
   () => {
     showUnloadingErrorMessage('Не удалось загрузить данные из сервера :(');
   },
